75c737b6be9d396e0da2dad2bc3b13e2c298163c
OleSiclops <sos@flowsystemsusa.com>
2025-04-26 14:11:28 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 # config.py
 
+import streamlit as st
+
 COINGECKO_API_BASE = "https://api.coingecko.com/api/v3"
 TOP_N_COINS = 50
-COINGECKO_API_KEY = "CG-rbpNtj6G8LXoFT6W9BjgRPGS"
+COINGECKO_API_KEY = st.secrets["general"]["COINGECKO_API_KEY"]
